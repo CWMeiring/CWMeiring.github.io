17f4794a6cd52770d925932aefb4c057d160fe37
Christo Meiring <32700954+CWMeiring@users.noreply.github.com>
2018-03-07 18:26:43 +0200

    Add files via upload

---
+++ logo.js
@@ -0,0 +1,45 @@
+if (screen.width <= 699) {
+document.location = "mobi.html";
+}
+
+function wu(){
+	var pone =
+	document.getElementById("pconone");
+	pone.style.display = "none";
+
+	var ptwo =
+	document.getElementById("pcontwo");
+	ptwo.style.display = "block";
+
+	var pthree =
+	document.getElementById("pconthree");
+	pthree.style.display = "none";
+}
+
+function home(){
+	var pone =
+	document.getElementById("pconone");
+	pone.style.display = "block";
+
+	var ptwo =
+	document.getElementById("pcontwo");
+	ptwo.style.display = "none";
+	
+	var pthree =
+	document.getElementById("pconthree");
+	pthree.style.display = "none";
+}
+
+function contact(){
+	var pone =
+	document.getElementById("pconone");
+	pone.style.display = "none";
+
+	var ptwo =
+	document.getElementById("pcontwo");
+	ptwo.style.display = "none";
+	
+	var pthree =
+	document.getElementById("pconthree");
+	pthree.style.display = "block";
+}
---
+++ logom.js
@@ -0,0 +1,41 @@
+function wu(){
+	var pone =
+	document.getElementById("pconone");
+	pone.style.display = "none";
+
+	var ptwo =
+	document.getElementById("pcontwo");
+	ptwo.style.display = "block";
+
+	var pthree =
+	document.getElementById("pconthree");
+	pthree.style.display = "none";
+}
+
+function home(){
+	var pone =
+	document.getElementById("pconone");
+	pone.style.display = "block";
+
+	var ptwo =
+	document.getElementById("pcontwo");
+	ptwo.style.display = "none";
+	
+	var pthree =
+	document.getElementById("pconthree");
+	pthree.style.display = "none";
+}
+
+function contact(){
+	var pone =
+	document.getElementById("pconone");
+	pone.style.display = "none";
+
+	var ptwo =
+	document.getElementById("pcontwo");
+	ptwo.style.display = "none";
+	
+	var pthree =
+	document.getElementById("pconthree");
+	pthree.style.display = "block";
+}
