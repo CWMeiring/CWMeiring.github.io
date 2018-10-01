cedb134f66df9dd056a7612397ee2b807af99827
Christo Meiring <32700954+CWMeiring@users.noreply.github.com>
2018-10-01 11:51:44 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+	<link rel ="stylesheet"
+		  type ="text/css"
+		  href ="test.css">
+	
+	<script src="test.js"></script>
+	<title></title>
+	
+</head>
+
+<body>
+	<div id="main">
+	<p id ="p1">email me<p>
+	<form id="contact-form" action="contactform.php" method="post">
+	<input type="text" name="name" placeholder="Full name" id="inname">
+	<input type="text" name="mail" placeholder="Your e-mail" id="inmail">
+	<input type="text" name="subject" placeholder="subject" id="insub">
+	<textarea name ="message" placeholder="message" id="txt"></textarea>
+	<button type="submit" name="submit" id="button1">SEND</button>
+	</form>
+	<div>
+</body>
+
+</html>
