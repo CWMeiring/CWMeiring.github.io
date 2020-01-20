5e27210ba9f7c009a2654e313183bd56e3a8ff04
Christo Meiring <32700954+CWMeiring@users.noreply.github.com>
2020-01-20 16:18:40 +0200

    Add files via upload

---
+++ carousel.js
@@ -0,0 +1,541 @@
+var plusMinus = 0;
+
+/*LEFT*/
+function Left(){
+	var jsLeftRight = 
+document.getElementById("javascript");
+	var cssLeftRight =
+document.getElementById("css");
+	var htmlLeftRight =
+document.getElementById("html");
+	var jqLeftRight =
+document.getElementById("jquery");
+	var phpLeftRight =
+document.getElementById("php");
+	var sqlLeftRight =
+document.getElementById("sql");
+	var leftArow =
+document.getElementById("left");
+	var leftArowGrey =
+document.getElementById("leftGrey");
+    
+	var rightArow =
+document.getElementById("right");
+var rightArowGrey = document.getElementById('rightGrey');
+
+	++plusMinus
+	
+	if(plusMinus == 0){
+/*Javascript*/
+jsLeftRight.style.width = "48vw";
+jsLeftRight.style.height = "33vw";
+jsLeftRight.style.marginTop = "0vw";
+jsLeftRight.style.marginLeft = "10vw";
+jsLeftRight.style.zIndex = "3";	
+/*css*/
+cssLeftRight.style.width = "26vw";
+cssLeftRight.style.height = "18vw";
+cssLeftRight.style.marginTop = "7vw";
+cssLeftRight.style.marginLeft = "3vw";
+cssLeftRight.style.zIndex = "2";
+/*html*/	
+htmlLeftRight.style.width = "16vw";
+htmlLeftRight.style.height = "12vw";
+htmlLeftRight.style.marginTop = "10vw";
+htmlLeftRight.style.marginLeft = "0vw";
+htmlLeftRight.style.zIndex = "1";
+/*jQuery*/
+jqLeftRight.style.width = "26vw";
+jqLeftRight.style.height = "18vw";
+jqLeftRight.style.marginTop = "7vw";
+jqLeftRight.style.marginLeft = "39vw";
+jqLeftRight.style.zIndex = "2";	
+/*php*/
+phpLeftRight.style.width = "16vw";
+phpLeftRight.style.height = "12vw";
+phpLeftRight.style.marginTop = "10vw";
+phpLeftRight.style.marginLeft = "52vw";
+phpLeftRight.style.zIndex = "1";
+/*sql*/
+sqlLeftRight.style.width = "16vw";
+sqlLeftRight.style.height = "12vw";
+sqlLeftRight.style.marginTop = "10vw";
+sqlLeftRight.style.marginLeft = "52vw";
+sqlLeftRight.style.zIndex = "0";};
+    
+	if(plusMinus == 1){
+/*Javascript*/
+jsLeftRight.style.width = "26vw";
+jsLeftRight.style.height = "18vw";
+jsLeftRight.style.marginTop = "7vw";
+jsLeftRight.style.marginLeft = "39vw";
+jsLeftRight.style.zIndex = "2";	
+/*css*/
+cssLeftRight.style.width = "48vw";
+cssLeftRight.style.height = "33vw";
+cssLeftRight.style.marginTop = "0vw";
+cssLeftRight.style.marginLeft = "10vw";
+cssLeftRight.style.zIndex = "3";
+/*html*/	
+htmlLeftRight.style.width = "26vw";
+htmlLeftRight.style.height = "18vw";
+htmlLeftRight.style.marginTop = "7vw";
+htmlLeftRight.style.marginLeft = "3vw";
+htmlLeftRight.style.zIndex = "2";
+/*jQuery*/
+jqLeftRight.style.width = "16vw";
+jqLeftRight.style.height = "12vw";
+jqLeftRight.style.marginTop = "10vw";
+jqLeftRight.style.marginLeft = "52vw";
+jqLeftRight.style.zIndex = "1";
+/*php*/
+phpLeftRight.style.width = "16vw";
+phpLeftRight.style.height = "12vw";
+phpLeftRight.style.marginTop = "10vw";
+phpLeftRight.style.marginLeft = "52vw";
+phpLeftRight.style.zIndex = "0";
+/*sql*/
+sqlLeftRight.style.width = "16vw";
+sqlLeftRight.style.height = "12vw";
+sqlLeftRight.style.marginTop = "10vw";
+sqlLeftRight.style.marginLeft = "52vw";
+sqlLeftRight.style.zIndex = "0";};
+    
+	if(plusMinus == 2){
+/*Javascript*/
+jsLeftRight.style.width = "16vw";
+jsLeftRight.style.height = "12vw";
+jsLeftRight.style.marginTop = "10vw";
+jsLeftRight.style.marginLeft = "52vw";
+jsLeftRight.style.zIndex = "1";	
+/*css*/
+cssLeftRight.style.width = "26vw";
+cssLeftRight.style.height = "18vw";
+cssLeftRight.style.marginTop = "7vw";
+cssLeftRight.style.marginLeft = "39vw";
+cssLeftRight.style.zIndex = "2";
+/*html*/	
+htmlLeftRight.style.width = "48vw";
+htmlLeftRight.style.height = "33vw";
+htmlLeftRight.style.marginTop = "0vw";
+htmlLeftRight.style.marginLeft = "10vw";
+htmlLeftRight.style.zIndex = "3";
+/*jQuery*/
+jqLeftRight.style.width = "16vw";
+jqLeftRight.style.height = "12vw";
+jqLeftRight.style.marginTop = "10vw";
+jqLeftRight.style.marginLeft = "52vw";
+jqLeftRight.style.zIndex = "0";
+/*php*/
+phpLeftRight.style.width = "16vw";
+phpLeftRight.style.height = "12vw";
+phpLeftRight.style.marginTop = "10vw";
+phpLeftRight.style.marginLeft = "52vw";
+phpLeftRight.style.zIndex = "0";
+/*sql*/
+sqlLeftRight.style.width = "16vw";
+sqlLeftRight.style.height = "12vw";
+sqlLeftRight.style.marginTop = "10vw";
+sqlLeftRight.style.marginLeft = "52vw";
+sqlLeftRight.style.zIndex = "0";
+/*Left Arow*/
+leftArow.style.display ="none";	
+leftArowGrey.style.display ="block";					
+	}
+if(plusMinus == -1){
+/*Javascript*/
+jsLeftRight.style.width = "26vw";
+jsLeftRight.style.height = "18vw";
+jsLeftRight.style.marginTop = "7vw";
+jsLeftRight.style.marginLeft = "3vw";
+jsLeftRight.style.zIndex = "2";	
+/*css*/
+cssLeftRight.style.width = "16vw";
+cssLeftRight.style.height = "12vw";
+cssLeftRight.style.marginTop = "10vw";
+cssLeftRight.style.marginLeft = "0vw";
+cssLeftRight.style.zIndex = "1";
+/*html*/	
+htmlLeftRight.style.width = "16vw";
+htmlLeftRight.style.height = "12vw";
+htmlLeftRight.style.marginTop = "10vw";
+htmlLeftRight.style.marginLeft = "0vw";
+htmlLeftRight.style.zIndex = "0";
+/*jQuery*/
+jqLeftRight.style.width = "48vw";
+jqLeftRight.style.height = "33vw";
+jqLeftRight.style.marginTop = "0vw";
+jqLeftRight.style.marginLeft = "10vw";
+jqLeftRight.style.zIndex = "3";
+/*php*/
+phpLeftRight.style.width = "26vw";
+phpLeftRight.style.height = "18vw";
+phpLeftRight.style.marginTop = "7vw";
+phpLeftRight.style.marginLeft = "39vw";
+phpLeftRight.style.zIndex = "2";
+/*sql*/
+sqlLeftRight.style.width = "16vw";
+sqlLeftRight.style.height = "12vw";
+sqlLeftRight.style.marginTop = "10vw";
+sqlLeftRight.style.marginLeft = "52vw";
+sqlLeftRight.style.zIndex = "1"; "1";					
+	}
+if(plusMinus == -2){
+/*Javascript*/
+jsLeftRight.style.width = "16vw";
+jsLeftRight.style.height = "12vw";
+jsLeftRight.style.marginTop = "10vw";
+jsLeftRight.style.marginLeft = "0vw";
+jsLeftRight.style.zIndex = "1";	
+/*css*/
+cssLeftRight.style.width = "16vw";
+cssLeftRight.style.height = "12vw";
+cssLeftRight.style.marginTop = "10vw";
+cssLeftRight.style.marginLeft = "0vw";
+cssLeftRight.style.zIndex = "0";
+/*html*/	
+htmlLeftRight.style.width = "16vw";
+htmlLeftRight.style.height = "12vw";
+htmlLeftRight.style.marginTop = "10vw";
+htmlLeftRight.style.marginLeft = "0vw";
+htmlLeftRight.style.zIndex = "0";
+/*jQuery*/
+jqLeftRight.style.width = "26vw";
+jqLeftRight.style.height = "18vw";
+jqLeftRight.style.marginTop = "7vw";
+jqLeftRight.style.marginLeft = "3vw";
+jqLeftRight.style.zIndex = "2";
+/*php*/
+phpLeftRight.style.width = "48vw";
+phpLeftRight.style.height = "33vw";
+phpLeftRight.style.marginTop = "0vw";
+phpLeftRight.style.marginLeft = "10vw";
+phpLeftRight.style.zIndex = "3";
+/*sql*/
+sqlLeftRight.style.width = "26vw";
+sqlLeftRight.style.height = "18vw";
+sqlLeftRight.style.marginTop = "7vw";
+sqlLeftRight.style.marginLeft = "39vw";
+sqlLeftRight.style.zIndex = "2";
+
+/*Right Arow*/
+rightArow.style.display ="block";	
+rightArowGrey.style.display ="none";					
+	}
+if(plusMinus == -3){
+/*Javascript*/
+jsLeftRight.style.width = "16vw";
+jsLeftRight.style.height = "12vw";
+jsLeftRight.style.marginTop = "10vw";
+jsLeftRight.style.marginLeft = "0vw";
+jsLeftRight.style.zIndex = "0";	
+/*css*/
+cssLeftRight.style.width = "16vw";
+cssLeftRight.style.height = "12vw";
+cssLeftRight.style.marginTop = "10vw";
+cssLeftRight.style.marginLeft = "0vw";
+cssLeftRight.style.zIndex = "0";
+/*html*/	
+htmlLeftRight.style.width = "16vw";
+htmlLeftRight.style.height = "12vw";
+htmlLeftRight.style.marginTop = "10vw";
+htmlLeftRight.style.marginLeft = "0vw";
+htmlLeftRight.style.zIndex = "0";
+/*jQuery*/
+jqLeftRight.style.width = "16vw";
+jqLeftRight.style.height = "12vw";
+jqLeftRight.style.marginTop = "10vw";
+jqLeftRight.style.marginLeft = "0vw";
+jqLeftRight.style.zIndex = "1";
+/*php*/
+phpLeftRight.style.width = "26vw";
+phpLeftRight.style.height = "18vw";
+phpLeftRight.style.marginTop = "7vw";
+phpLeftRight.style.marginLeft = "3vw";
+phpLeftRight.style.zIndex = "2";
+/*sql*/
+sqlLeftRight.style.width = "48vw";
+sqlLeftRight.style.height = "33vw";
+sqlLeftRight.style.marginTop = "0vw";
+sqlLeftRight.style.marginLeft = "10vw";
+sqlLeftRight.style.zIndex = "3";
+/*Right Arow*/
+rightArow.style.display ="none";	
+rightArowGrey.style.display ="block";					
+	}
+};
+
+/*RIGHT*/
+function Right(){
+	var jsLeftRight = 
+document.getElementById("javascript");
+	var cssLeftRight =
+document.getElementById("css");
+	var htmlLeftRight =
+document.getElementById("html");
+	var jqLeftRight =
+document.getElementById("jquery");
+	var phpLeftRight =
+document.getElementById("php");
+	var sqlLeftRight =
+document.getElementById("sql");
+	var rightArow =
+document.getElementById("right");
+	var rightArowGrey =
+document.getElementById("rightGrey");
+	var leftArow =
+document.getElementById("left");
+	var leftArowGrey =
+document.getElementById("leftGrey");
+
+	--plusMinus
+	
+	if(plusMinus == 0){
+/*Javascript*/
+jsLeftRight.style.width = "48vw";
+jsLeftRight.style.height = "33vw";
+jsLeftRight.style.marginTop = "0vw";
+jsLeftRight.style.marginLeft = "10vw";
+jsLeftRight.style.zIndex = "3";	
+/*css*/
+cssLeftRight.style.width = "26vw";
+cssLeftRight.style.height = "18vw";
+cssLeftRight.style.marginTop = "7vw";
+cssLeftRight.style.marginLeft = "3vw";
+cssLeftRight.style.zIndex = "2";
+/*html*/	
+htmlLeftRight.style.width = "16vw";
+htmlLeftRight.style.height = "12vw";
+htmlLeftRight.style.marginTop = "10vw";
+htmlLeftRight.style.marginLeft = "0vw";
+htmlLeftRight.style.zIndex = "1";
+/*jQuery*/
+jqLeftRight.style.width = "26vw";
+jqLeftRight.style.height = "18vw";
+jqLeftRight.style.marginTop = "7vw";
+jqLeftRight.style.marginLeft = "39vw";
+jqLeftRight.style.zIndex = "2";	
+/*php*/
+phpLeftRight.style.width = "16vw";
+phpLeftRight.style.height = "12vw";
+phpLeftRight.style.marginTop = "10vw";
+phpLeftRight.style.marginLeft = "52vw";
+phpLeftRight.style.zIndex = "1";
+/*sql*/
+sqlLeftRight.style.width = "16vw";
+sqlLeftRight.style.height = "12vw";
+sqlLeftRight.style.marginTop = "10vw";
+sqlLeftRight.style.marginLeft = "52vw";
+sqlLeftRight.style.zIndex = "0";	
+	};
+	if(plusMinus == 1){
+/*Javascript*/
+jsLeftRight.style.width = "26vw";
+jsLeftRight.style.height = "18vw";
+jsLeftRight.style.marginTop = "7vw";
+jsLeftRight.style.marginLeft = "39vw";
+jsLeftRight.style.zIndex = "2";	
+/*css*/
+cssLeftRight.style.width = "48vw";
+cssLeftRight.style.height = "33vw";
+cssLeftRight.style.marginTop = "0vw";
+cssLeftRight.style.marginLeft = "10vw";
+cssLeftRight.style.zIndex = "3";
+/*html*/	
+htmlLeftRight.style.width = "26vw";
+htmlLeftRight.style.height = "18vw";
+htmlLeftRight.style.marginTop = "7vw";
+htmlLeftRight.style.marginLeft = "3vw";
+htmlLeftRight.style.zIndex = "2";
+/*jQuery*/
+jqLeftRight.style.width = "16vw";
+jqLeftRight.style.height = "12vw";
+jqLeftRight.style.marginTop = "10vw";
+jqLeftRight.style.marginLeft = "52vw";
+jqLeftRight.style.zIndex = "1";
+/*php*/
+phpLeftRight.style.width = "16vw";
+phpLeftRight.style.height = "12vw";
+phpLeftRight.style.marginTop = "10vw";
+phpLeftRight.style.marginLeft = "52vw";
+phpLeftRight.style.zIndex = "0";
+/*sql*/
+sqlLeftRight.style.width = "16vw";
+sqlLeftRight.style.height = "12vw";
+sqlLeftRight.style.marginTop = "10vw";
+sqlLeftRight.style.marginLeft = "52vw";
+sqlLeftRight.style.zIndex = "0";		
+	};
+leftArow.style.display ="block";	
+leftArowGrey.style.display ="none";
+	if(plusMinus == 2){
+/*Javascript*/
+jsLeftRight.style.width = "16vw";
+jsLeftRight.style.height = "12vw";
+jsLeftRight.style.marginTop = "10vw";
+jsLeftRight.style.marginLeft = "52vw";
+jsLeftRight.style.zIndex = "1";	
+/*css*/
+cssLeftRight.style.width = "26vw";
+cssLeftRight.style.height = "18vw";
+cssLeftRight.style.marginTop = "7vw";
+cssLeftRight.style.marginLeft = "39vw";
+cssLeftRight.style.zIndex = "2";
+/*html*/	
+htmlLeftRight.style.width = "48vw";
+htmlLeftRight.style.height = "33vw";
+htmlLeftRight.style.marginTop = "0vw";
+htmlLeftRight.style.marginLeft = "10vw";
+htmlLeftRight.style.zIndex = "3";
+/*jQuery*/
+jqLeftRight.style.width = "16vw";
+jqLeftRight.style.height = "12vw";
+jqLeftRight.style.marginTop = "10vw";
+jqLeftRight.style.marginLeft = "52vw";
+jqLeftRight.style.zIndex = "0";
+/*php*/
+phpLeftRight.style.width = "16vw";
+phpLeftRight.style.height = "12vw";
+phpLeftRight.style.marginTop = "10vw";
+phpLeftRight.style.marginLeft = "52vw";
+phpLeftRight.style.zIndex = "0";
+/*sql*/
+sqlLeftRight.style.width = "16vw";
+sqlLeftRight.style.height = "12vw";
+sqlLeftRight.style.marginTop = "10vw";
+sqlLeftRight.style.marginLeft = "52vw";
+sqlLeftRight.style.zIndex = "0";
+/*Left Arow*/
+leftArow.style.display ="none";	
+leftArowGrey.style.display ="block";					
+	}
+if(plusMinus == -1){
+/*Javascript*/
+jsLeftRight.style.width = "26vw";
+jsLeftRight.style.height = "18vw";
+jsLeftRight.style.marginTop = "7vw";
+jsLeftRight.style.marginLeft = "3vw";
+jsLeftRight.style.zIndex = "2";	
+/*css*/
+cssLeftRight.style.width = "16vw";
+cssLeftRight.style.height = "12vw";
+cssLeftRight.style.marginTop = "10vw";
+cssLeftRight.style.marginLeft = "0vw";
+cssLeftRight.style.zIndex = "1";
+/*html*/	
+htmlLeftRight.style.width = "16vw";
+htmlLeftRight.style.height = "12vw";
+htmlLeftRight.style.marginTop = "10vw";
+htmlLeftRight.style.marginLeft = "0vw";
+htmlLeftRight.style.zIndex = "0";
+/*jQuery*/
+jqLeftRight.style.width = "48vw";
+jqLeftRight.style.height = "33vw";
+jqLeftRight.style.marginTop = "0vw";
+jqLeftRight.style.marginLeft = "10vw";
+jqLeftRight.style.zIndex = "3";
+/*php*/
+phpLeftRight.style.width = "26vw";
+phpLeftRight.style.height = "18vw";
+phpLeftRight.style.marginTop = "7vw";
+phpLeftRight.style.marginLeft = "39vw";
+phpLeftRight.style.zIndex = "2";
+/*sql*/
+sqlLeftRight.style.width = "16vw";
+sqlLeftRight.style.height = "12vw";
+sqlLeftRight.style.marginTop = "10vw";
+sqlLeftRight.style.marginLeft = "52vw";
+sqlLeftRight.style.zIndex = "1"; "1";					
+	}
+if(plusMinus == -2){
+/*Javascript*/
+jsLeftRight.style.width = "16vw";
+jsLeftRight.style.height = "12vw";
+jsLeftRight.style.marginTop = "10vw";
+jsLeftRight.style.marginLeft = "0vw";
+jsLeftRight.style.zIndex = "1";	
+/*css*/
+cssLeftRight.style.width = "16vw";
+cssLeftRight.style.height = "12vw";
+cssLeftRight.style.marginTop = "10vw";
+cssLeftRight.style.marginLeft = "0vw";
+cssLeftRight.style.zIndex = "0";
+/*html*/	
+htmlLeftRight.style.width = "16vw";
+htmlLeftRight.style.height = "12vw";
+htmlLeftRight.style.marginTop = "10vw";
+htmlLeftRight.style.marginLeft = "0vw";
+htmlLeftRight.style.zIndex = "0";
+/*jQuery*/
+jqLeftRight.style.width = "26vw";
+jqLeftRight.style.height = "18vw";
+jqLeftRight.style.marginTop = "7vw";
+jqLeftRight.style.marginLeft = "3vw";
+jqLeftRight.style.zIndex = "2";
+/*php*/
+phpLeftRight.style.width = "48vw";
+phpLeftRight.style.height = "33vw";
+phpLeftRight.style.marginTop = "0vw";
+phpLeftRight.style.marginLeft = "10vw";
+phpLeftRight.style.zIndex = "3";
+/*sql*/
+sqlLeftRight.style.width = "26vw";
+sqlLeftRight.style.height = "18vw";
+sqlLeftRight.style.marginTop = "7vw";
+sqlLeftRight.style.marginLeft = "39vw";
+sqlLeftRight.style.zIndex = "2";
+
+/*Right Arow*/
+rightArow.style.display ="block";	
+rightArowGrey.style.display ="none";					
+	}
+if(plusMinus == -3){
+/*Javascript*/
+jsLeftRight.style.width = "16vw";
+jsLeftRight.style.height = "12vw";
+jsLeftRight.style.marginTop = "10vw";
+jsLeftRight.style.marginLeft = "0vw";
+jsLeftRight.style.zIndex = "0";	
+/*css*/
+cssLeftRight.style.width = "16vw";
+cssLeftRight.style.height = "12vw";
+cssLeftRight.style.marginTop = "10vw";
+cssLeftRight.style.marginLeft = "0vw";
+cssLeftRight.style.zIndex = "0";
+/*html*/	
+htmlLeftRight.style.width = "16vw";
+htmlLeftRight.style.height = "12vw";
+htmlLeftRight.style.marginTop = "10vw";
+htmlLeftRight.style.marginLeft = "0vw";
+htmlLeftRight.style.zIndex = "0";
+/*jQuery*/
+jqLeftRight.style.width = "16vw";
+jqLeftRight.style.height = "12vw";
+jqLeftRight.style.marginTop = "10vw";
+jqLeftRight.style.marginLeft = "0vw";
+jqLeftRight.style.zIndex = "1";
+/*php*/
+phpLeftRight.style.width = "26vw";
+phpLeftRight.style.height = "18vw";
+phpLeftRight.style.marginTop = "7vw";
+phpLeftRight.style.marginLeft = "3vw";
+phpLeftRight.style.zIndex = "2";
+/*sql*/
+sqlLeftRight.style.width = "48vw";
+sqlLeftRight.style.height = "33vw";
+sqlLeftRight.style.marginTop = "0vw";
+sqlLeftRight.style.marginLeft = "10vw";
+sqlLeftRight.style.zIndex = "3";
+/*Right Arow*/
+rightArow.style.display ="none";	
+rightArowGrey.style.display ="block";					
+	}
+};
+
+
+
+
+
+
+
+
