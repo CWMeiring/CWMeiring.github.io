75856b552516b047ee155b815143c2e8f9f6a099
Christo Meiring <32700954+CWMeiring@users.noreply.github.com>
2020-01-23 10:56:25 +0200

    Add files via upload

---
+++ mobijs.js
@@ -0,0 +1,10 @@
+
+function displaydropdown(){
+	var disdropdown = document.getElementById("dropdown");
+		disdropdown.style.display = "block";
+};
+
+function hidedropdown(){
+	var disdropdown = document.getElementById("dropdown");
+		disdropdown.style.display = "none";
+};
