01796d1719b94a2ea22e807bff7c4330b5e0c0c3
Christo Meiring <32700954+CWMeiring@users.noreply.github.com>
2020-01-24 10:25:54 +0200

    Add files via upload

---
+++ mobijs.js
@@ -0,0 +1,47 @@
+
+function displaydropdown(){
+	var disdropdown = document.getElementById("dropdown");
+		disdropdown.style.display = "block";
+};
+
+function hidedropdown(){
+	var disdropdown = document.getElementById("dropdown");
+		disdropdown.style.display = "none";
+};
+
+function Home(){
+	document.documentElement.scrollTop = 0;
+	var disdropdown = document.getElementById("dropdown");
+		disdropdown.style.display = "none";
+}
+
+function About(){
+	document.documentElement.scrollTop = +1900;
+	var disdropdown = document.getElementById("dropdown");
+		disdropdown.style.display = "none";
+}
+
+function Skill(){
+	document.documentElement.scrollTop = +1900;
+	var disdropdown = document.getElementById("dropdown");
+		disdropdown.style.display = "none";
+}
+
+function Education(){
+	document.documentElement.scrollTop = +4050;
+	var disdropdown = document.getElementById("dropdown");
+		disdropdown.style.display = "none";
+}
+
+function Employment(){
+	document.documentElement.scrollTop = +5950;
+	var disdropdown = document.getElementById("dropdown");
+		disdropdown.style.display = "none";
+}
+
+function Contackt(){
+	document.documentElement.scrollTop = +8650;
+	var disdropdown = document.getElementById("dropdown");
+		disdropdown.style.display = "none";
+}
+
